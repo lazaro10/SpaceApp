8e2c35186b5fa94a56e109487d05ee2c757649c7
Andre Rodrigues <andre.msr@icloud.com>
2018-02-26 22:22:57 -0300

    added references to child view controllers in news view controller

--- SpaceApp/SpaceApp/Scenes/NewsViewController.swift
+++ SpaceApp/SpaceApp/Scenes/NewsViewController.swift
@@ -10,32 +10,45 @@ import UIKit
 import Rswift
 
 protocol NewsViewControllerOutput {
-    
-    func fetchNews(request: NewsModel.FetchNews.Request)
+  
+  func fetchNews(request: NewsModel.FetchNews.Request)
 }
 
 class NewsViewController: UIViewController {
+  
+  var output: NewsViewControllerOutput?
+  weak var videoViewController: VideoViewController?
+  weak var descriptionViewController: DescriptionViewController?
+  weak var actionsViewController: ActionsViewController?
+  weak var relatedVideosViewController: RelatedVideosViewController?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    NewsConfigurator.configure(viewController: self)
+    output?.fetchNews(request: NewsModel.FetchNews.Request())
+  }
+  
+  func displayNews(viewModel: NewsModel.FetchNews.ViewModel) {
+  
+  }
+  
+  func displayError(error: NewsModel.FetchNews.Error.ViewModel) {
     
-    var output: NewsViewControllerOutput?
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        NewsConfigurator.configure(viewController: self)
-        output?.fetchNews(request: NewsModel.FetchNews.Request())
+    switch segue.destination {
+    case let videoViewController as VideoViewController:
+      self.videoViewController = videoViewController
+    case let descriptionViewController as DescriptionViewController:
+      self.descriptionViewController = descriptionViewController
+    case let actionsViewController as ActionsViewController:
+      self.actionsViewController = actionsViewController
+    case let relatedVideosViewController as RelatedVideosViewController:
+      self.relatedVideosViewController = relatedVideosViewController
+    default:
+      break
     }
-    
-    func displayNews(viewModel: NewsModel.FetchNews.ViewModel) {
-        injectDescriptionContainer(bla: viewModel.newsDescription)
-    }
-    
-    func displayError(error: NewsModel.FetchNews.Error.ViewModel) {
-        
-    }
-    
-    private func injectDescriptionContainer(bla: String) {
-        let descriptionContainer: DescriptionViewController = R.storyboard.main().instantiateInitialViewController() as! DescriptionViewController
-        
-        descriptionContainer.teste = bla
-    }
+  }
 }
